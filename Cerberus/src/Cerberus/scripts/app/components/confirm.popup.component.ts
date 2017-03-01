e1318c43849f83bc70a4df5abac840ff8afe4bd7
Konrad Janowski <velotricon@gmail.com>
2017-03-01 06:44:25 +0100

    daily commit

---
+++ Cerberus/src/Cerberus/scripts/app/components/confirm.popup.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component, Output, EventEmitter }        from '@angular/core';
+
+@Component({
+    selector: 'confirm-popup',
+    templateUrl: './templates/confirm.popup.html'
+})
+export class ConfirmPopupComponent {
+    
+    @Output('OnOkClick') OnOkClick = new EventEmitter();
+    @Output('OnAbortClick') OnAbortClick = new EventEmitter();
+
+    private is_visible = false;
+    private text: string;
+    private ShowOkAbortButtons = true;
+    private ShowYesNoButtons = false;
+
+    private ok_click(): void {
+        this.is_visible = false;
+        this.OnOkClick.emit();
+    }
+    private abort_click(): void {
+        this.is_visible = false;
+        this.OnAbortClick.emit();
+    }
+
+    public Show(Text: string) {
+        this.text = Text;
+        this.is_visible = true;
+    }
+    public Show(Text: string, YesNoMode: boolean): void {
+
+    }
+
+}
